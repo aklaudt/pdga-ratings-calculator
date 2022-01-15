078afd04a1ead779f57d7f85c2dc7de87cb10363
Andrew Klaudt <Andrew.Klaudt@mines.sdsmt.edu>
2022-01-14 22:40:43 -0600

    Added all members boi

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 from pdgaRequests import pdgaRequests
 from database import Database
 from htmlParsing import HtmlParser
-from utils import ingestNewMembers
+from utils import ingestNewMembers, ingestRatedRoundsForActiveMembers
 from models import RatedRound
 
 # pdgaNumber = 1
@@ -14,6 +14,6 @@ conn = Database.createConnection("./pdga.db")
 
 start_time = time.time()
 numberOfPlayersToIngest = 150000
-ingestNewMembers(conn, numberOfPlayersToIngest)
+ingestRatedRoundsForActiveMembers(conn)
 print("--- %s seconds ---" % (time.time() - start_time))
 print((time.time() - start_time) / numberOfPlayersToIngest)
